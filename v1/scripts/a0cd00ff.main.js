f8a6d9cded6c91294afa2a23ea8e935b132250ac
Chris Gunawardena <chris@gunawardena.id.au>
2016-04-13 22:00:39 +0200

    Added old version

---
+++ v1/scripts/a0cd00ff.main.js
@@ -0,0 +1,1 @@
+var add_to_home=addToHomescreen({maxDisplayCount:3,startDelay:15,appID:"puevfthanjneqran",mandatory:!1}),myScroll=new IScroll("#wrapper",{scrollX:!0,scrollY:!0,momentum:!1,snap:!0});document.addEventListener("touchmove",function(a){a.preventDefault()},!1);
